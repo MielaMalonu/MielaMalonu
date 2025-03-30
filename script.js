2562b497940fb535e2859563a6969a6cccff3e33
AuraxoGT <135165346+AuraxoGT@users.noreply.github.com>
2025-03-30 15:24:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", async function () {
             CLIENT_ID: "1263389179249692693",
             REDIRECT_URI: window.location.origin + window.location.pathname,
             SCOPES: ["identify", "guilds.members.read"],
-            WEBHOOK_URL: "https://discord.com/api/webhooks/1346529699081490472/k-O-v4wKDiUjsj1w-Achvrej1Kr-W-rXqZVibcftwWFn5sMZyhIMSb9E4r975HbQI3tF",
+            WEBHOOK_URL: "https://canary.discord.com/api/webhooks/1346529699081490472/k-O-v4wKDiUjsj1w-Achvrej1Kr-W-rXqZVibcftwWFn5sMZyhIMSb9E4r975HbQI3tF",
             GUILD_ID: "1325850250027597845"
         }
     };
